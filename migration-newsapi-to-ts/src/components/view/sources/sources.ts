77958500d5bae39d5675ec2fc789c84164576c8f
kaliganoff <kaliganoff@yahoo.com>
2024-02-26 13:38:59 +0300

    refactor: Edit sources types

--- migration-newsapi-to-ts/src/components/view/sources/sources.ts
+++ migration-newsapi-to-ts/src/components/view/sources/sources.ts
@@ -3,19 +3,20 @@ import { Source } from 'interfaces';
 
 class Sources {
     draw(data: Source[]) {
-        const fragment = document.createDocumentFragment();
-        const sourceItemTemp = document.querySelector('#sourceItemTemp') as HTMLTemplateElement;
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
 
         data.forEach((item: Source) => {
-            const sourceClone: HTMLElement = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+            if (sourceItemTemp) {
+              const sourceClone: HTMLTemplateElement = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
+              sourceClone.querySelector('.source__item-name')!.textContent = item.name;
+              sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
 
-            sourceClone.querySelector('.source__item-name')!.textContent = item.name;
-            sourceClone.querySelector('.source__item')!.setAttribute('data-source-id', item.id);
-
-            fragment.append(sourceClone);
+              fragment.append(sourceClone);
+            }
         });
 
-        document.querySelector('.sources')!.append(fragment);
+        document.querySelector('.sources')?.append(fragment);
     }
 }
 
