5ece6448746cbfcdbd1dd35417696da3a604451b
kaliganoff <kaliganoff@yahoo.com>
2024-02-26 14:15:31 +0300

    feat: Add new variables and edit types in sources and news

--- migration-newsapi-to-ts/src/components/view/news/news.ts
+++ migration-newsapi-to-ts/src/components/view/news/news.ts
@@ -1,37 +1,42 @@
+import { Article } from '../../../interfaces';
 import './news.css';
 
 class News {
-    draw(data) {
-        const news = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
+    draw(data: Article[]): void {
+        const news: Article[] = data.length >= 10 ? data.filter((_item, idx) => idx < 10) : data;
 
-        const fragment = document.createDocumentFragment();
-        const newsItemTemp = document.querySelector('#newsItemTemp');
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const newsItemTemp: HTMLTemplateElement = document.querySelector('#newsItemTemp') as HTMLTemplateElement;
 
         news.forEach((item, idx) => {
-            const newsClone = newsItemTemp.content.cloneNode(true);
+            const newsClone: HTMLElement = newsItemTemp.content.cloneNode(true) as HTMLElement;
 
-            if (idx % 2) newsClone.querySelector('.news__item').classList.add('alt');
+            if (idx % 2) newsClone.querySelector('.news__item')?.classList.add('alt');
 
-            newsClone.querySelector('.news__meta-photo').style.backgroundImage = `url(${
-                item.urlToImage || 'img/news_placeholder.jpg'
-            })`;
-            newsClone.querySelector('.news__meta-author').textContent = item.author || item.source.name;
-            newsClone.querySelector('.news__meta-date').textContent = item.publishedAt
-                .slice(0, 10)
-                .split('-')
-                .reverse()
-                .join('-');
-
-            newsClone.querySelector('.news__description-title').textContent = item.title;
-            newsClone.querySelector('.news__description-source').textContent = item.source.name;
-            newsClone.querySelector('.news__description-content').textContent = item.description;
-            newsClone.querySelector('.news__read-more a').setAttribute('href', item.url);
+            const newsMetaPhoto: HTMLElement | null = newsClone.querySelector('.news__meta-photo');
+            if (newsMetaPhoto) {
+                newsMetaPhoto.style.backgroundImage = `url(${item.urlToImage || 'img/news_placeholder.jpg'})`;
+            }
+            const newsMetaAuthor: HTMLElement | null = newsClone.querySelector('.news__meta-author');
+            newsMetaAuthor ? (newsMetaAuthor.textContent = item.author || item.source.name) : undefined;
+            const newsMetaDate: HTMLElement | null = newsClone.querySelector('.news__meta-date');
+            newsMetaDate
+                ? (newsMetaDate.textContent = item.publishedAt.slice(0, 10).split('-').reverse().join('-'))
+                : undefined;
+            const newsDescriptionTitle: HTMLElement | null = newsClone.querySelector('.news__description-title');
+            const newsDescriptionSource: HTMLElement | null = newsClone.querySelector('.news__description-source');
+            const newsDescriptionContent: HTMLElement | null = newsClone.querySelector('.news__description-content');
+            newsDescriptionTitle ? (newsDescriptionTitle.textContent = item.title) : undefined;
+            newsDescriptionSource ? (newsDescriptionSource.textContent = item.source.name) : undefined;
+            newsDescriptionContent ? (newsDescriptionContent.textContent = item.description) : undefined;
+            newsClone.querySelector('.news__read-more a')?.setAttribute('href', item.url);
 
             fragment.append(newsClone);
         });
 
-        document.querySelector('.news').innerHTML = '';
-        document.querySelector('.news').appendChild(fragment);
+        const newsElement: HTMLElement | null = document.querySelector('.news');
+        newsElement ? (newsElement.innerHTML = '') : undefined;
+        document.querySelector('.news')?.appendChild(fragment);
     }
 }
 
--- migration-newsapi-to-ts/src/components/view/sources/sources.ts
+++ migration-newsapi-to-ts/src/components/view/sources/sources.ts
@@ -2,17 +2,20 @@ import './sources.css';
 import { Source } from 'interfaces';
 
 class Sources {
-    draw(data: Source[]) {
+    draw(data: Source[]): void {
         const fragment: DocumentFragment = document.createDocumentFragment();
         const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
 
         data.forEach((item: Source) => {
             if (sourceItemTemp) {
-              const sourceClone: HTMLTemplateElement = sourceItemTemp.content.cloneNode(true) as HTMLTemplateElement;
-              sourceClone.querySelector('.source__item-name')!.textContent = item.name;
-              sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
+                const sourceClone: HTMLElement = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+                const sourceItemName: HTMLElement | null = sourceClone.querySelector('.source__item-name');
+                if (sourceItemName) {
+                    sourceItemName.textContent = item.name;
+                }
+                sourceClone.querySelector('.source__item')?.setAttribute('data-source-id', item.id);
 
-              fragment.append(sourceClone);
+                fragment.append(sourceClone);
             }
         });
 
