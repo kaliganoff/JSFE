d68a0510e80e6be387682527e983fcf92b6d0fb9
kaliganoff <96372655+kaliganoff@users.noreply.github.com>
2024-03-10 22:37:35 +0300

    Chore: RSS-PZ-00 Configure the Project (#28)
    * chore: RSS-PZ-00 Configure webpack
    
    * chore: RSS-PZ-00 Configure prettier
    
    * chore: RSS-PZ-00 Configure eslint
    
    * chore: RSS-PZ-00 Add .eslintignore and the lint script to package.json
    
    * chore: RSS-PZ-00 Configure husky and reinstall typescript plugin as a devdependency
    
    * chore: RSS-PZ-00 Add eslint:recommended and typescript-eslint/recommended

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const EslintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    publicPath: "auto",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  plugins: [new EslintPlugin({ extensions: "ts" })],
+};
