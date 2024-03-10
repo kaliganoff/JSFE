fc6f3be7ebfa68ef05de2ca46a0306510ea2d23c
kaliganoff <kaliganoff@yahoo.com>
2024-03-10 17:27:34 +0300

    chore: RSS-PZ-00 Configure prettier

--- rss-puzzle/webpack.config.js
+++ rss-puzzle/webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    devtool: 'eval-source-map',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                include: [path.resolve(__dirname, 'src')]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.js']
-    },
-    output: {
-        publicPath: 'auto',
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    }
-}
+  devtool: "eval-source-map",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  output: {
+    publicPath: "auto",
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
