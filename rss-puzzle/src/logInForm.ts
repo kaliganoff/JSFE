170191ce7beeb6375a8bc0afbe14f2717e195896
kaliganoff <96372655+kaliganoff@users.noreply.github.com>
2024-03-11 16:14:42 +0300

    feat: RSS-PZ-01 Create the login form (#29)

---
+++ rss-puzzle/src/logInForm.ts
@@ -0,0 +1,33 @@
+export default function drawLogInForm() {
+  const logInForm = document.createElement("form");
+  logInForm.className = "log-in-form";
+  const nameInput = document.createElement("input");
+  nameInput.id = "name";
+  nameInput.required = true;
+  const nameLabel: HTMLLabelElement = document.createElement("label");
+  nameLabel.innerText = "Name";
+  nameLabel.htmlFor = "name";
+  const surnameInput = document.createElement("input");
+  surnameInput.id = "surname";
+  surnameInput.required = true;
+  nameInput.className = "log-in-input";
+  surnameInput.className = "log-in-input";
+  const surnameLabel = document.createElement("label");
+  surnameLabel.innerText = "Surname";
+  surnameLabel.htmlFor = "surname";
+  const logInButton = document.createElement("button");
+  logInButton.innerText = "Log In";
+  logInButton.type = "submit";
+  logInButton.className = "log-in-button";
+
+  document.body.append(logInForm);
+  logInForm.append(nameLabel);
+  logInForm.append(nameInput);
+  logInForm.append(surnameLabel);
+  logInForm.append(surnameInput);
+  logInForm.append(logInButton);
+
+  logInForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+}
