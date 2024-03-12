722c105f8895c20ceb61001e397d8e888cd4c343
kaliganoff <96372655+kaliganoff@users.noreply.github.com>
2024-03-12 20:12:55 +0300

    feat: RSS-PZ-06 Implement a simple greeting (#35)

--- rss-puzzle/src/startScreen.ts
+++ rss-puzzle/src/startScreen.ts
@@ -3,6 +3,9 @@ startScreen.className = "start-screen";
 const appName = document.createElement("p");
 appName.className = "app-name";
 appName.innerText = "RSS PUZZLE";
+const greeting = document.createElement("p");
+const user = JSON.parse(localStorage.user);
+greeting.innerText = `Hi, ${user.name} ${user.surname}, and welcome to RSS PUZZLE!`;
 const appDescription = document.createElement("p");
 appDescription.innerText =
   "This is a puzzle where you need to construct English sentences from words.";
@@ -10,5 +13,6 @@ appDescription.innerText =
 export default function drawStartScreen() {
   document.body.append(startScreen);
   startScreen.append(appName);
+  startScreen.append(greeting);
   startScreen.append(appDescription);
 }
