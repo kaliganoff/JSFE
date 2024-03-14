64a07eccbcbc1311be8c7d199c342ecbc483dbb0
kaliganoff <kaliganoff@yahoo.com>
2024-03-14 14:54:48 +0300

    feat: RSS-PZ-07 Add start button + improve the greeting

--- rss-puzzle/src/startScreen.ts
+++ rss-puzzle/src/startScreen.ts
@@ -1,18 +1,28 @@
+import drawGame from "./game";
+
 const startScreen = document.createElement("div");
 startScreen.className = "start-screen";
 const appName = document.createElement("p");
 appName.className = "app-name";
 appName.innerText = "RSS PUZZLE";
 const greeting = document.createElement("p");
-const user = JSON.parse(localStorage.user);
-greeting.innerText = `Hi, ${user.name} ${user.surname}, and welcome to RSS PUZZLE!`;
 const appDescription = document.createElement("p");
 appDescription.innerText =
   "This is a puzzle where you need to construct English sentences from words.";
+const startButton = document.createElement("button");
+startButton.className = "start-button";
+startButton.innerText = "Start!";
+startButton.addEventListener("click", () => {
+  startScreen.remove();
+  drawGame();
+});
 
-export default function drawStartScreen() {
+export default function drawStartScreen(userPassed: string) {
+  const user = JSON.parse(userPassed);
+  greeting.innerText = `Hi, ${user.name} ${user.surname}, and welcome to RSS PUZZLE!`;
   document.body.append(startScreen);
   startScreen.append(appName);
   startScreen.append(greeting);
   startScreen.append(appDescription);
+  startScreen.append(startButton);
 }
