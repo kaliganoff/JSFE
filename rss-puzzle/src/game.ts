9fdafad56d281fef990c07dec92f92d358e2d25a
kaliganoff <kaliganoff@yahoo.com>
2024-03-15 00:40:28 +0300

    feat: RSS-PZ-08, RSS-PZ-10 Add word cards in random order, add moving from block to block

--- rss-puzzle/src/game.ts
+++ rss-puzzle/src/game.ts
@@ -1,1 +1,36 @@
-export default function drawGame() {}
+import wordCollectionLevel1 from "./wordCollectionLevel1.json";
+
+const gameContainer = document.createElement("div");
+gameContainer.className = "game-container";
+const resultBlock = document.createElement("div");
+resultBlock.className = "sentence-block";
+const sourceBlock = document.createElement("div");
+sourceBlock.className = "sentence-block";
+
+const sentence = wordCollectionLevel1.rounds[0].words[0].textExample
+  .split(" ")
+  .sort(() => Math.random() - 0.5);
+
+function moveWord(wordBlock: HTMLParagraphElement) {
+  if (wordBlock.parentElement === sourceBlock) {
+    resultBlock.append(wordBlock);
+  } else {
+    sourceBlock.append(wordBlock);
+  }
+}
+
+export default function drawGame() {
+  document.body.append(gameContainer);
+  gameContainer.append(resultBlock);
+  gameContainer.append(sourceBlock);
+
+  sentence.forEach((word) => {
+    const wordBlock = document.createElement("p");
+    wordBlock.className = "word-block";
+    wordBlock.addEventListener("click", () => {
+      moveWord(wordBlock);
+    });
+    wordBlock.innerText = word;
+    sourceBlock.append(wordBlock);
+  });
+}
