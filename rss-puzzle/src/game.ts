22455b230c377999ed757133ecfe5b787a9711b7
kaliganoff <kaliganoff@yahoo.com>
2024-03-16 00:13:42 +0300

    feat: RSS-PZ-11 Implement continue button and switching sentences in Level1

--- rss-puzzle/src/game.ts
+++ rss-puzzle/src/game.ts
@@ -6,25 +6,48 @@ const resultBlock = document.createElement("div");
 resultBlock.className = "sentence-block";
 const sourceBlock = document.createElement("div");
 sourceBlock.className = "sentence-block";
+const continueButton = document.createElement("button");
+continueButton.innerText = "Continue";
+continueButton.disabled = true;
 
-const sentence = wordCollectionLevel1.rounds[0].words[0].textExample
-  .split(" ")
-  .sort(() => Math.random() - 0.5);
+let roundNumber = 0;
+let wordNumber = 0;
+let sentence = "";
+let sentenceMixed: string[] = [];
+
+function initiateSentence() {
+  sentence =
+    wordCollectionLevel1.rounds[roundNumber].words[wordNumber].textExample;
+  sentenceMixed = sentence.split(" ").sort(() => Math.random() - 0.5);
+}
+
+initiateSentence();
+
+function checkSentence() {
+  const result: string[] = [];
+  for (let i = 0; i < resultBlock.children.length; i += 1) {
+    result.push(resultBlock.children[i].innerHTML);
+  }
+  const sentenceIsRight = result.join(" ") === sentence;
+  if (sentenceIsRight) {
+    continueButton.disabled = false;
+  } else {
+    continueButton.disabled = true;
+  }
+}
 
 function moveWord(wordBlock: HTMLParagraphElement) {
   if (wordBlock.parentElement === sourceBlock) {
     resultBlock.append(wordBlock);
+    checkSentence();
   } else {
     sourceBlock.append(wordBlock);
+    checkSentence();
   }
 }
 
-export default function drawGame() {
-  document.body.append(gameContainer);
-  gameContainer.append(resultBlock);
-  gameContainer.append(sourceBlock);
-
-  sentence.forEach((word) => {
+function fillSourceBlock() {
+  sentenceMixed.forEach((word) => {
     const wordBlock = document.createElement("p");
     wordBlock.className = "word-block";
     wordBlock.addEventListener("click", () => {
@@ -32,5 +55,30 @@ export default function drawGame() {
     });
     wordBlock.innerText = word;
     sourceBlock.append(wordBlock);
+  });
+}
+
+function continueGame() {
+  wordNumber += 1;
+  if (wordNumber > 9) {
+    roundNumber += 1;
+    wordNumber = 0;
+  }
+  initiateSentence();
+  resultBlock.innerHTML = "";
+  sourceBlock.innerHTML = "";
+  fillSourceBlock();
+}
+
+export default function drawGame() {
+  document.body.append(gameContainer);
+  gameContainer.append(resultBlock);
+  gameContainer.append(sourceBlock);
+
+  fillSourceBlock();
+
+  gameContainer.append(continueButton);
+  continueButton.addEventListener("click", () => {
+    continueGame();
   });
 }
