7163f57e92f8e72697e401676334dbc1352ec2c9
kaliganoff <kaliganoff@yahoo.com>
2024-03-26 12:48:29 +0300

    feat: Add car images and color to them

--- async-race/src/api.ts
+++ async-race/src/api.ts
@@ -1,104 +1,103 @@
 export async function getCars() {
-    let response = await fetch('http://localhost:3000/garage')
-    let result = await response.json();
-    return result;
+  const response = await fetch('http://localhost:3000/garage');
+  const result = await response.json();
+  return result;
 }
-  
-  function getCar(id: number) {
-    fetch(`http://localhost:3000/garage/${id}`)
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
- export async function createCar(car) {
-    fetch('http://localhost:3000/garage', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(car),
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function deleteCar(id: number) {
-    fetch(`http://localhost:3000/garage/${id}`, {
-      method: 'DELETE',
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function updateCar(id: number, car) {
-    fetch(`http://localhost:3000/garage/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(car),
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function startStopEngine(id: number, status) {
-    fetch(`http://localhost:3000/engine?id=${id}&status=${status}`, {
-      method: 'PATCH',
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function driveMode(id: number) {
-    fetch(`http://localhost:3000/engine?id=${id}&status=drive`, {
-      method: 'PATCH',
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
+
+/* function getCar(id: number) {
+  fetch(`http://localhost:3000/garage/${id}`)
+    .then((response) => response.json())
+    .then(console.log);
+}
+*/
+export async function createCar(car: { name: string, color: string}) {
+  fetch('http://localhost:3000/garage', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(car),
+  })
+    .then((response) => response.json())
+    .then(console.log);
+}
+/*
+function deleteCar(id: number) {
+  fetch(`http://localhost:3000/garage/${id}`, {
+    method: 'DELETE',
+  })
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function updateCar(id: number, car) {
+  fetch(`http://localhost:3000/garage/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(car),
+  })
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function startStopEngine(id: number, status) {
+  fetch(`http://localhost:3000/engine?id=${id}&status=${status}`, {
+    method: 'PATCH',
+  })
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function driveMode(id: number) {
+  fetch(`http://localhost:3000/engine?id=${id}&status=drive`, {
+    method: 'PATCH',
+  })
+    .then((response) => response.json())
+    .then(console.log);
+} */
+
 export async function getWinners() {
-      let response = await fetch('http://localhost:3000/winners')
-      let result = await response.json();
-      return result;
-  }
-  
-  function getWinner(id: number) {
-    fetch(`http://localhost:3000/winners/${id}`)
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function createWinner(car) {
-    fetch('http://localhost:3000/winners', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(car),
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function deleteWinner(id: number) {
-    fetch(`http://localhost:3000/winners/${id}`, {
-      method: 'DELETE',
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
-  function updateWinner(id: number, car) {
-    fetch(`http://localhost:3000/winners/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(car),
-    })
-      .then((response) => response.json())
-      .then(console.log);
-  }
-  
+  const response = await fetch('http://localhost:3000/winners');
+  const result = await response.json();
+  return result;
+}
+
+/* function getWinner(id: number) {
+  fetch(`http://localhost:3000/winners/${id}`)
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function createWinner(car) {
+  fetch('http://localhost:3000/winners', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(car),
+  })
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function deleteWinner(id: number) {
+  fetch(`http://localhost:3000/winners/${id}`, {
+    method: 'DELETE',
+  })
+    .then((response) => response.json())
+    .then(console.log);
+}
+
+function updateWinner(id: number, car) {
+  fetch(`http://localhost:3000/winners/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(car),
+  })
+    .then((response) => response.json())
+    .then(console.log);
+} */
--- async-race/src/main.ts
+++ async-race/src/main.ts
@@ -1,33 +1,33 @@
-import "./style.css";
-import * as API from './api'
+import './style.css';
+import * as API from './api';
 
-const viewDiv = document.createElement("div");
-const garageButton = document.createElement("button");
-const winnersButton = document.createElement("button");
-garageButton.innerText = "TO GARAGE";
-winnersButton.innerText = "TO WINNERS";
+const viewDiv = document.createElement('div');
+const garageButton = document.createElement('button');
+const winnersButton = document.createElement('button');
+garageButton.innerText = 'TO GARAGE';
+winnersButton.innerText = 'TO WINNERS';
 
-const garageContainer = document.createElement("div");
+const garageContainer = document.createElement('div');
 
-const garageHeader = document.createElement("p");
-garageHeader.innerText = "GARAGE";
+const garageHeader = document.createElement('p');
+garageHeader.innerText = 'GARAGE';
 
-const numberOfCars = document.createElement("p");
-let getCarsResult: [] = await API.getCars();
+const numberOfCars = document.createElement('p');
+const getCarsResult: [] = await API.getCars();
 numberOfCars.innerText = `${getCarsResult.length}`;
 
 const createCarForm = document.createElement('form');
-const nameInput = document.createElement("input");
+const nameInput = document.createElement('input');
 const colorInput = document.createElement('input');
 colorInput.type = 'color';
-const createCarButton = document.createElement("button");
+const createCarButton = document.createElement('button');
 createCarButton.innerText = 'CREATE CAR';
 
 const updateCarForm = document.createElement('form');
-const updateNameInput = document.createElement("input");
+const updateNameInput = document.createElement('input');
 const updateColorInput = document.createElement('input');
 updateColorInput.type = 'color';
-const updateCarButton = document.createElement("button");
+const updateCarButton = document.createElement('button');
 updateCarButton.innerText = 'UPDATE CAR';
 
 garageContainer.append(garageHeader);
@@ -37,11 +37,11 @@ createCarForm.append(nameInput, colorInput, createCarButton);
 garageContainer.append(updateCarForm);
 updateCarForm.append(updateNameInput, updateColorInput, updateCarButton);
 
-const winnersContainer = document.createElement("div");
-const winnersHeader = document.createElement("p");
-winnersHeader.innerText = "WINNERS";
-const numberOfWinners = document.createElement("p");
-let getWinnersResult: [] = await API.getWinners();
+const winnersContainer = document.createElement('div');
+const winnersHeader = document.createElement('p');
+winnersHeader.innerText = 'WINNERS';
+const numberOfWinners = document.createElement('p');
+const getWinnersResult: [] = await API.getWinners();
 numberOfWinners.innerText = `${getWinnersResult.length}`;
 winnersContainer.append(winnersHeader);
 winnersContainer.append(numberOfWinners);
@@ -53,6 +53,20 @@ viewDiv.append(winnersButton);
 document.body.append(garageContainer);
 document.body.append(winnersContainer);
 
+getCarsResult.forEach((car: { name: string, color: string }) => {
+  const carContainer = document.createElement('div');
+  const carName = document.createElement('p');
+  carName.innerText = car.name;
+  const carIMG = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  const carIMGUse = document.createElementNS('http://www.w3.org/2000/svg', 'use');
+  carIMGUse.setAttribute('href', 'transport.svg#car');
+  carIMG.style.fill = car.color;
+  carIMG.append(carIMGUse);
+  carContainer.append(carName);
+  carContainer.append(carIMG);
+  garageContainer.append(carContainer);
+});
+
 function openGarage() {
   winnersContainer.hidden = true;
   garageContainer.hidden = false;
@@ -63,13 +77,12 @@ function openWinners() {
   garageContainer.hidden = true;
 }
 
-garageButton.addEventListener("click", openGarage);
-winnersButton.addEventListener("click", openWinners);
+garageButton.addEventListener('click', openGarage);
+winnersButton.addEventListener('click', openWinners);
 
 createCarForm.addEventListener('submit', () => {
   API.createCar({
-  name: nameInput.value,
-  color: colorInput.value
-})
-}
-);
+    name: nameInput.value,
+    color: colorInput.value,
+  });
+});
